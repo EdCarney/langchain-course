b94f3445c097aaf988625a99c04d5da14c915b2f
Ed Carney <carneyedwardj@gmail.com>
2026-09-13 00:19:35 -0400

    Adds structured output to the model via pydantic

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 
 load_dotenv()
+from pydantic import BaseModel, Field
 from langchain.agents import create_agent
 from langchain.tools import tool
 from langchain_anthropic import ChatAnthropic
@@ -11,6 +12,16 @@ from tavily import TavilyClient
 
 tavily = TavilyClient()
 
+class Source(BaseModel):
+    """Schema for a data source used by an agent."""
+
+    url: str = Field("The URL of the source.")
+
+class AgentResponse(BaseModel):
+    """Schema for an agent response."""
+
+    answer: str = Field("The agent's answer to the query.")
+    sources: list[Source] = Field(default_factory=list, description="A list of sources used by the agent to generate the answer.")
 
 @tool
 def search(query: str) -> str:
@@ -28,7 +39,7 @@ def search(query: str) -> str:
 def main():
     llm = ChatOllama(model="gemma4:e4b-mlx")
     llm = ChatAnthropic(model="claude-sonnet-5")
-    agent = create_agent(model=llm, tools=[TavilySearch()])
+    agent = create_agent(model=llm, tools=[TavilySearch()], response_format=AgentResponse)
     agent_data = {
         "messages": HumanMessage(
             content="Search for 3 job postings for a LangChain AI engineer in the Bay Area. Please also list all of their related details."
